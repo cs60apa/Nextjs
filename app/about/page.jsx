6e3ebec62c4a39f35c9a619b86765c96246852fa
marksikaundi <emmanuelmark117@gmail.com>
2024-10-10 16:19:07 +0200

    Refactor global CSS styles and add layout components
    - Refactored the global CSS styles in `globals.css` to use CSS variables for background and foreground colors.
    - Added `Header` and `Footer` components to the layout in `layout.js`.
    - Updated the title and description in the `metadata` object in `layout.js` to reflect the Next.js tutorial.
    - Created the `About` and `Contact` pages with corresponding components.
    - Added the `HeroSection` component to the `About` page.
    - Created the `HeroContact` component for the `Contact` page.
    - Added the `Footer` and `Header` components to their respective files.
    - Created the `HeroSection` component with a donation button and some placeholder content.

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+
+// import HeroSection from '@/components/Hero-section'
+import HeroSection from '@/components/Hero-section'
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <HeroSection/>
+      Hello about me
+    </div>
+  )
+}
---
+++ app/contact/page.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import HeroContact from './here-contact'
+
+export default function Contact() {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <p>
+        Hello this is contacts us
+      </p>
+      <HeroContact/>
+    </div>
+  )
+}
